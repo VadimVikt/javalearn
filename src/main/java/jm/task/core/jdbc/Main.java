f1ac08eefae5d02c8fc2a0a037927da8ab1defa0
Vadim Bridnya <v-bridnya@yandex.ru>
2020-08-14 12:42:41 +0300

    task 1.1.4 невозможно соединиться с базой

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -14,26 +14,27 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserServiceImpl userService = new UserServiceImpl();
+        Util.getSessionFactory();
+//        UserServiceImpl userService = new UserServiceImpl();
         //Создание таблицы
-        userService.createUsersTable();
-        //Добавление 4 юзеров в таблицу
-        List<User> users = new ArrayList<>();
-        users.add(new User("Jhon", "Smith", (byte) 32));
-        users.add(new User("Ivan", "Petrov", (byte) 18));
-        users.add(new User("Li-Si-Cin", "Лисицын", (byte) 48));
-        users.add(new User("Александр", "Sidorov", (byte) 55));
-        for (User u : users) {
-            userService.saveUser(u.getName(), u.getLastName(), u.getAge());
-        }
-        //Получение всех юзеров и вывод в консоль
-        List<User> us =  userService.getAllUsers();
-        for (User u : us) {
-            System.out.println(u.toString());
-        }
-        //Очистка таблицы
-        userService.cleanUsersTable();
-        //Удаление таблицы
-        userService.dropUsersTable();
+//        userService.createUsersTable();
+//        //Добавление 4 юзеров в таблицу
+//        List<User> users = new ArrayList<>();
+//        users.add(new User("Jhon", "Smith", (byte) 32));
+//        users.add(new User("Ivan", "Petrov", (byte) 18));
+//        users.add(new User("Li-Si-Cin", "Лисицын", (byte) 48));
+//        users.add(new User("Александр", "Sidorov", (byte) 55));
+//        for (User u : users) {
+//            userService.saveUser(u.getName(), u.getLastName(), u.getAge());
+//        }
+//        //Получение всех юзеров и вывод в консоль
+//        List<User> us =  userService.getAllUsers();
+//        for (User u : us) {
+//            System.out.println(u.toString());
+//        }
+//        //Очистка таблицы
+//        userService.cleanUsersTable();
+//        //Удаление таблицы
+//        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,59 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Hibernate;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.StandardServiceInitiators;
+
+//import javax.security.auth.login.AppConfigurationEntry;
+//import javax.security.auth.login.Configuration;
+import java.lang.annotation.Annotation;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    //В класс Util должна быть добавлена конфигурация для Hibernate ( рядом с JDBC)
+    //Настройки Hibernate
+
+    private static SessionFactory sessionFactory = null;
+    private static ServiceRegistry serviceRegistry;
+
+    static  {
+        try {
+            Properties prop = new Properties();
+            prop.put(Environment.URL,"jdbc:mysql://localhost:3306/javalearn");
+            prop.put(Environment.USER,"mysql" );
+            prop.put(Environment.PASS, "mysql");
+            prop.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+            prop.put(Environment.SHOW_SQL, true);
+            prop.put(Environment.HBM2DDL_AUTO, "update");
+            prop.put(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+
+            Configuration configuration = new Configuration().setProperties(prop);
+            System.out.println(configuration.getProperties());
+            configuration.addAnnotatedClass(User.class);
+            // через интерфейс ServiceRegistry и потом класс
+//                serviceRegistry = new StandardServiceRegistryBuilder().applySetting(configuration.getProperties()).build();
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+//            System.out.println("Исключение!" + e);
+                e.printStackTrace();
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
     public static Connection getMySQLConnection() throws SQLException {
         String hostName = "localhost";
